581050128a3bc4c40e4fa0ceb734824bab55f94f
Mahesh Makani <mahesh.makani@netnatives.co.uk>
2017-12-30 13:27:13 +0000

    update homepage

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,15 +1,50 @@
 import React from 'react'
-import Typography from 'material-ui/Typography'
+import { Typography, Paper, Hidden } from 'material-ui'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
 
-const Home = () => (
-  <div>
-    <Typography type='display4' component='h1'>
-      Habrewken
-    </Typography>
-    <Typography type='display1' component='h2'>
-      Brighton Fighting Game Community Website and Resource
-    </Typography>
-  </div>
+import './Home.css'
+
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    paddingTop: 16,
+    paddingBottom: 16,
+    height: '100%',
+    background:
+      `linear-gradient(
+        rgba(0, 0, 0, 0.7), 
+        rgba(0, 0, 0, 0.7)
+      ),
+      url("https://res.cloudinary.com/mkn-sh/image/upload/f_auto/v1514624353/fgc/hbk.jpg");`,
+    backgroundPosition: 'left',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
+  })
+})
+
+const Home = ({classes}) => (
+  <Paper className={classes.root} elevation={0}>
+    <Hidden smUp>
+      <Typography type='display2' component='h1'>
+        Habrewken
+      </Typography>
+      <Typography type='headline' component='h2'>
+        Brighton Fighting Game Community - Website and Resource
+      </Typography>
+    </Hidden>
+    <Hidden smDown>
+      <Typography type='display4' component='h1'>
+        Habrewken
+      </Typography>
+      <Typography type='display1' component='h2'>
+        Brighton Fighting Game Community - Website and Resource
+      </Typography>
+    </Hidden>
+  </Paper>
 )
 
-export default Home
+Home.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Home)
--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,29 +1,12 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Switch, Route } from 'react-router-dom'
-import { withStyles } from 'material-ui/styles'
-import Paper from 'material-ui/Paper'
 
 import Home from './Home/Home'
 
-const styles = theme => ({
-  root: theme.mixins.gutters({
-    paddingTop: 16,
-    paddingBottom: 16,
-    height: '100%'
-  })
-})
-
-const Main = ({classes}) => (
-  <Paper className={classes.root} elevation={0}>
-    <Switch>
-      <Route exact path='/' component={Home} />
-    </Switch>
-  </Paper>
+const Main = () => (
+  <Switch>
+    <Route exact path='/' component={Home} />
+  </Switch>
 )
 
-Main.propTypes = {
-  classes: PropTypes.object.isRequired
-}
-
-export default withStyles(styles)(Main)
+export default Main
