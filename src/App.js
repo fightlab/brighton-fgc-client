c4589ebcd1aa3282237562f858ebf03df6936b71
Mahesh Makani <mahesh.makani@netnatives.co.uk>
2017-12-30 10:16:19 +0000

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import Route from 'react-router-dom/Route';
-import Switch from 'react-router-dom/Switch';
-import Home from './Home';
 import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
 
-const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-  </Switch>
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    paddingTop: 16,
+    paddingBottom: 16,
+    margin: theme.spacing.unit * 3,
+  }),
+});
+
+const App = ({classes}) => (
+  <Paper className={classes.root} elevation={4}>
+    <Typography type="headline" component="h3">
+      This is a sheet of paper.
+    </Typography>
+    <Typography type="body1" component="p">
+      Paper can be used to build surface or other elements for your application.
+    </Typography>
+  </Paper>
 );
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
